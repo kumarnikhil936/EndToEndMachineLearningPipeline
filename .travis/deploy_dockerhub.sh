253d5aee754198cef9fc78f6bb3586e754bc6c4c
Nikhil Kumar Jha <kumar.nikhil936@gmail.com>
2020-12-27 23:34:48 +0100

    Updating docker login method

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+cat access_token_docker.txt | docker login --username $DOCKER_USER --password-stdin
+#docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+echo $TRAVIS_BRANCH
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   TAG="latest"
 else
   TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_USER/buzz:$TAG
+
+echo $TAG
+
+docker build -f Dockerfile -t $DOCKER_USER/buzz:$TAG .
 docker push $DOCKER_USER/buzz:$TAG
